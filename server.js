a34d82eeaee4479fa7526a2fd6b2aa115250bb1f
gabepp <gabrielporaj@gmail.com>
2025-02-25 00:00:57 +0000

    d

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'docs')));
 app.use(express.json());
 
